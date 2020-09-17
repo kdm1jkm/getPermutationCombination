2c84bfcba2d65aea0a91fb7f0e30bb168525c4ed
kdm <kdm1jkmm@gmail.com>
2020-09-17 10:51:09 +0900

    Add getPermutation()
    This code doesn't consider whether list contains duplicate thing.

--- src/main/java/Main.kt
+++ src/main/java/Main.kt
@@ -1,3 +1,34 @@
 fun main() {
-    println("Hello, World!")
+    val result = getPermutation(listOf(1, 2, 3), 3)
+    result.forEach {
+        println(it.toString())
+    }
+}
+
+fun <T> getPermutation(source: List<T>, count: Int): List<List<T>> {
+    when {
+        count < 0 -> {
+            throw IndexOutOfBoundsException()
+        }
+        count == 0 -> {
+            return listOf(emptyList())
+        }
+        count == 1 -> {
+            return source.map { listOf(it) }
+        }
+    }
+
+    val result: MutableList<List<T>> = ArrayList()
+    source.forEachIndexed { i, element ->
+        val otherPermutations = getPermutation(source.filterIndexed { index, _ -> index != i }, count - 1)
+        val combinedPermutations = otherPermutations.map {
+            it.toMutableList()
+        }
+        combinedPermutations.forEach {
+            it.add(0, element)
+        }
+        result.addAll(combinedPermutations)
+    }
+
+    return result
 }
